062fb98d08cce24b8cff3ec07f47ba3c18e715d7
Nicholas Teo <nicholasteock@gmail.com>
2014-06-06 11:41:46 +0800

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Base Setup
 
 // Call required packages
-var http = require('http');
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var router = express.Router();
@@ -17,10 +17,102 @@ app.use(bodyParser());
 var port = process.env.PORT || 8080;
 
 // Routes for API
-
 router.get('/', function(req, res) {
 	res.json({ message: "HELLO FROM THE API!" });
 });
+
+/***USERS*********************************************************************/
+
+router.get('/users', function( req, res ) {
+	var queryString = 'SELECT * FROM user WHERE accessToken="' + req.query.access_token + '"';
+
+	var connection = mysql.createConnection({
+		host: 'askitdb.cvumcgqvkpk0.us-west-2.rds.amazonaws.com',
+		user: 'nicholasteo',
+		password: 'nicholasteo',
+		database: 'askitdb',
+		port: 3306
+	});
+
+	connection.connect();
+	connection.query( queryString, function( err, rows ) {
+		var response = { result: "", data: {} };
+
+		if( err ) {
+			response.result = "failure";
+			response.error = "Query returned error.";
+			res.send(response);
+		}
+		else {
+			response.result = "success";
+			response.data = rows;
+			res.send(response);
+		}
+	});
+	connection.end();
+})
+
+/***QUESTIONS*****************************************************************/
+
+router.get('/questions', function(req, res) {
+	var connection = mysql.createConnection({
+		host: 'askitdb.cvumcgqvkpk0.us-west-2.rds.amazonaws.com',
+		user: 'nicholasteo',
+		password: 'nicholasteo',
+		database: 'askitdb',
+		port: 3306
+	});
+
+	connection.connect();
+	connection.query( 'SELECT * FROM question WHERE author=2', function( err, rows ) {
+		var response = { result: "", data: {} };
+
+		if( err ) {
+			response.result = "failure";
+			response.error = "Query returned error.";
+			res.send(response);
+		}
+		else {
+			response.result = "success";
+			response.data.questions = rows;
+			response.data.count = rows.length;
+			res.send(response);
+		}
+	});
+	connection.end();
+});
+
+/***ANSWERS*******************************************************************/
+
+router.get('/answers', function( req, res ) {
+	var connection = mysql.createConnection({
+		host: 'askitdb.cvumcgqvkpk0.us-west-2.rds.amazonaws.com',
+		user: 'nicholasteo',
+		password: 'nicholasteo',
+		database: 'askitdb',
+		port: 3306
+	});
+
+	connection.connect();
+	connection.query( 'SELECT * FROM answer WHERE author=2', function( err, rows ) {
+		var response = { result: "", data: {} };
+
+		if( err ) {
+			response.result = "failure";
+			response.error = "Query returned error.";
+			res.send(response);
+		}
+		else {
+			response.result = "success";
+			response.data.answers = rows;
+			response.data.count = rows.length;
+			res.send(response);
+		}
+	});
+	connection.end();
+});
+
+/******************************************************************************/
 
 // Register routes
 // all routes prefixed with /api
